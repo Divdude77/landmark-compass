466b5fce972527d451ad1f15aee1ae21c1e2b301
Divyanshu Verma <divyanshuverma91@gmail.com>
2024-06-19 14:00:40 +0400

    bruh moments

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
 const CACHE_NAME = 'compass-cache-v2';
 var urlsToCache = [
-    '/',
-    '/index.html',
-    '/css/style.css',
-    '/css/fontawesome.min.css',
-    '/css/solid.min.css',
-    '/script.js',
-    '/sw.js',
-    '/webfonts/fa-regular-400.ttf',
-    '/webfonts/fa-regular-400.woff2',
-    '/webfonts/fa-solid-900.ttf',
-    '/webfonts/fa-solid-900.woff2',
-    '/webfonts/fa-v4compatibility.ttf',
-    '/webfonts/fa-v4compatibility.woff2',
-    'manifest.json'
+    '.',
+    './index.html',
+    './css/style.css',
+    './css/fontawesome.min.css',
+    './css/solid.min.css',
+    './script.js',
+    './sw.js',
+    './webfonts/fa-regular-400.ttf',
+    './webfonts/fa-regular-400.woff2',
+    './webfonts/fa-solid-900.ttf',
+    './webfonts/fa-solid-900.woff2',
+    './webfonts/fa-v4compatibility.ttf',
+    './webfonts/fa-v4compatibility.woff2',
+    './manifest.json'
 ];
 
 self.addEventListener('install', event => {
